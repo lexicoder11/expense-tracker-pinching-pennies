d77d35d9f03c2c8c8499413a8adc898725fa865e
Lexi Allen <lexi.allen14@gmail.com>
2024-11-06 00:27:40 -0700

    set up backend folders

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet } from 'react-native'; // Remove Text component import
+import { View, StyleSheet } from 'react-native';
 import AppNavigator from './src/Navigation/AppNavigator';
 
 export default function App() {
